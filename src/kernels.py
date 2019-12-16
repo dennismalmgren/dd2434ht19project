8e3cd038ebf664ef87522f7b543462ef76009259
Li <liling@kth.se>
2019-12-17 00:26:10 +0900

    Add cluster kernel implementation

--- src/kernels.py
+++ src/kernels.py
@@ -2,6 +2,7 @@
 Module containing the implementation of various cluster kernels.
 """
 from functools import partial
+import numpy as np
 import gin
 from utils import LINEAR, LINEAR_STEP, STEP, POLY, POLY_STEP
 
@@ -11,7 +12,7 @@ class ClusterKernel:
     def __init__(self,
                  kernel_name=LINEAR,
                  degree=None,
-                 sigma=0,
+                 sigma=.55, #used by RBF kernel
                  cutoff_type = 'n_relative', #either n_relative or absolute
                  r = 10, #This defines cutoff for step and poly-step.
                  p=2, #Power for poly-step under or equal to cutoff
@@ -26,6 +27,7 @@ class ClusterKernel:
             - degree: required if 'poly' kernel is selected
             - num_labelled_points: required if 'poly-step' kernel is selected
         """
+        self.sigma = sigma
         self.kernel_name = kernel_name
         self.degree = degree
         self.num_labelled_points = num_labelled_points
@@ -58,12 +60,41 @@ class ClusterKernel:
         return selected_kernel
 
     def _compute_kernel(self, tf_func, data):
-        """ Step 1-4 of the proposed cluster kernel algorithm.
-        (should probably be divided into multiple methods.)
-        """
+        """ Step 1-4 of the proposed cluster kernel algorithm."""
+
+        # Step 1
+        matrix_K = self._rbf_kernel(data, self.sigma)
+        diag_K = np.sum(matrix_K, axis=1)
+        matrix_D = np.diag(diag_K)
+
+        # Step 2
+        diag_K_pow_neg_half = diag_K**(-.5)
+        matrix_D_pow_neg_half = np.diag(diag_K_pow_neg_half)
+        matrix_L = matrix_D_pow_neg_half @ matrix_K @ matrix_D_pow_neg_half
+        eig_vals, eig_vecs = np.linalg.eig(matrix_L)
+
+        # Step 3
+        lambda_eig_vals = tf_func(eig_vals)
+        lambda_L = eig_vecs @ np.diag(lambda_eig_vals) @ eig_vecs.T
+
+        # Step 4
+        diag_lambda_L = np.diagonal(lambda_L)
+        lambda_D = np.diag(1/diag_lambda_L)
+        lambda_K = lambda_D**(.5) @ lambda_L @ lambda_D**(.5)
+        return lambda_K
+
+    def _rbf_kernel(self, data, sigma):
+        """RBF kernel used by step 1 of the cluster kernel."""
+        # assumes data.shape = (N, D)
+        data_norm = np.linalg.norm(data, axis=1)**2
+        pairwise_distance = (data_norm.reshape(-1, 1)
+                             - 2*np.dot(data, data.T)
+                             + data_norm.reshape(1, -1))
+        return np.exp(-pairwise_distance/(2*sigma**2))
 
     def _linear_tf(self, lambda_):
         """Linear transfer function."""
+        return lambda_
 
     def _step_tf(self, lambda_):
         """Step transfer function."""
