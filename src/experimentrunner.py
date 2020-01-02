d69091469347e9820fd5977d118b49dfdedf2f0e
andreehultgren <andree_hultgren@hotmail.com>
2020-01-02 17:34:59 +0100

    Included plotting of results
    The figure_2 now computes the average of n random datapoints and plots it in a graph.

--- src/experimentrunner.py
+++ src/experimentrunner.py
@@ -4,6 +4,7 @@ import data_utils
 from svm import SVM
 import numpy as np
 from kernels import ClusterKernel
+import matplotlib.pyplot as plt
 
 @gin.configurable
 def train_svm(datasetLoader, test_points, data_limit=0):
@@ -40,25 +41,45 @@ class ExperimentRunner:
             kernel = ClusterKernel()
             input, output = datasetLoader.get_full_dataset()
             input, output = data_utils.construct_one_vs_all(input, output, 0)
+            
+            x_results = [2,4,8,16,32,64,128]
+            y_results = []
+            
+            for n_labeled_points in x_results:
+                print("Starting test for",n_labeled,"datapoints.")
+                results = 0
+                for i in range(100):
+                    print("Iteration #"+str(i))
 
-            (input_train, input_test, output_train, output_test) = data_utils.split(input, output, 768)
-            kernel_fun = kernel.kernel(input)
-            svm = SVM()
+                    kernel_fun = kernel.kernel(input)
+                    svm = SVM()
 
-            #Send the data and unlabeled data (testing is analysed as unlabeled data)
-            svm.set_kernel(kernel_fun)
+                    #Send the data and unlabeled data (testing is analysed as unlabeled data)
+                    svm.set_kernel(kernel_fun)
 
-            #Send the indexes of labeled training data and the labels
-            training_indexes = np.asarray(list(range(128)))
-            svm.give_training_data(training_indexes, output[0:128])
+                    #Get the training indexes
+                    training_indexes = np.asarray(list(range(128)))
+                    training_targets_subset=[]
+                    
+                    #Make sure that the data has both 1 and -1
+                    while 1 not in training_targets_subset or -1 not in training_targets_subset:
+                        training_indexes_subset = np.random.choice(training_indexes, n_labeled)
+                        training_targets_subset = output[training_indexes_subset]
+                    
+                    #Give the data to the SVM
+                    svm.give_training_data(training_indexes_subset, training_targets_subset)
 
-            #Train the SVM.
-            svm.train()
+                    #Train the SVM.
+                    svm.train()
 
-            #Send the indexes of labeled testing data and the labels
-            testing_indexes = np.asarray(list(range(128,256)))
-            svm.give_test_data(testing_indexes, output[128:256])
-            svm.analyze()
+                    #Send the indexes of labeled testing data and the labels
+                    testing_indexes = np.asarray(list(range(128,256)))
+                    svm.give_test_data(testing_indexes, output[128:256])
+                    misclassification = svm.analyze()
+                    results += misclassification
+                y_results.append(results/100)
+            plt.plot(x_results, y_results)
+            plt.show()
 
             #Todo: Construct kernel for each type of kernel function.
             #Todo: Average test error over 100 random selections of labelled points, ranging from 2->128.
