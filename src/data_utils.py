93b8d5777cc4e1a016f31ed7e8142cec5b3d49ea
Li <liling@kth.se>
2020-01-09 22:18:25 +0900

    Add tsvm experiments (some tweaking needed)

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,35 +1,45 @@
 import os
 import sklearn.model_selection as skm
-
 """
 Helper functions for manipulating files and directories (might need to be moved?)
 """
-
 """
 Helper function that splits input and output into to sets where the test_count parameter indicates how many
 samples to put in the second set from input and output.
 returns input_first_set, input_second_set, output_first_set, output_second_set
 """
+
+
 def split(input, output, test_count):
     return skm.train_test_split(input, output, test_size=test_count)
 
+
 """
-Adjusts target labels so that the sought after class has output label 1
-and every other item shares the same label of -1
+Adjusts target labels so that the sought after class has output label pos_label
+and every other item shares the same label of neg_label
 """
-def construct_one_vs_all(input, output, primary_target):
-    output[output != primary_target] = -1
-    output[output == primary_target] = 1
+
+
+def construct_one_vs_all(input,
+                         output,
+                         primary_target,
+                         pos_label=1,
+                         neg_label=-1):
+    output[output != primary_target] = neg_label
+    output[output == primary_target] = pos_label
     return input, output
-    
+
 
 """
 Returns the path to the datasets folder based on the location of utils.py
 """
+
+
 def get_datasets_folder():
-    scriptpath = os.path.realpath(__file__) # This refers to the utils file which needs to be in src for this to work. Should be enough for this simple project.
+    scriptpath = os.path.realpath(
+        __file__
+    )  # This refers to the utils file which needs to be in src for this to work. Should be enough for this simple project.
     cur_dir = os.path.dirname(scriptpath)
     base_dir = os.path.dirname(cur_dir)
     datasets_dir = os.path.join(base_dir, 'datasets')
     return datasets_dir
-
--- src/experimentrunner.py
+++ src/experimentrunner.py
@@ -6,10 +6,42 @@ import numpy as np
 from kernels import ClusterKernel
 import matplotlib.pyplot as plt
 from markov_random_walk import MRW
+import random
+
+# Import TSVM
+import importlib
+methods = importlib.import_module("semisup-learn.methods")
+from methods.scikitTSVM import SKTSVM as tsvm
+
+import warnings
+warnings.filterwarnings('ignore', category=PendingDeprecationWarning)
+
 
 @gin.configurable
-def train_tsvm(dataset_loader, test_points, data_limit=0):
-    pass
+def train_tsvm(dataset_loader, num_labels=16, num_iter=100):
+    # The TSVM package requires the following target outputs:
+    # 0, 1 = labels for labelled data
+    # -1 = unlabelled data
+    input_, output = dataset_loader.get_full_dataset()
+    pos_label, neg_label = 0, 1
+    input_, output = data_utils.construct_one_vs_all(
+        input_,
+        output,
+        primary_target=pos_label,
+        pos_label=pos_label,
+        neg_label=neg_label)
+
+    y_train = np.array([-1] * len(output))
+    print(input_.shape)
+    random_labeled_points = (
+        random.sample(list(np.where(output == pos_label)[0]), num_labels // 2) +
+        random.sample(list(np.where(output == neg_label)[0]), num_labels // 2))
+    y_train[random_labeled_points] = output[random_labeled_points]
+
+    tsvm_model = tsvm()
+    tsvm_model.fit(input_,
+                   y_train)
+    print(f"Transductive SVM score = {tsvm_model.score(input_, output)}")
 
 
 def get_data_up_to_limit(dataset_loader, data_limit):
@@ -35,6 +67,97 @@ def train_svm(dataset_loader, test_points, data_limit=0):
     svm.give_test_data(input_test, output_test)
     svm.analyze()
 
+def random_walk_experiment():
+    datasetLoader = NewsGroupDatasetLoader()
+    datasetLoader.load_dataset()
+    input, output = datasetLoader.get_full_dataset()
+    input, output = data_utils.construct_one_vs_all(input, output, 0)
+
+    n_labeled_points = 16
+
+    print("Starting test for ", n_labeled_points, " datapoints.")
+    results = 0
+    for i in range(100):
+        random_walk = MRW()
+
+        print("Iteration #" + str(i))
+
+        #Get the training indexes
+        training_indexes = np.asarray(list(range(128)))
+        training_targets_subset = []
+
+        #Make sure that the data has both 1 and -1
+        while 1 not in training_targets_subset or -1 not in training_targets_subset:
+            training_indexes_subset = np.random.choice(
+                training_indexes, n_labeled_points)
+            training_targets_subset = output[training_indexes_subset]
+
+        #Give the data to the random_walk
+        random_walk.give_training_data(
+            input[training_indexes_subset],
+            training_targets_subset)  # add input ?
+
+        # Adding the test values
+        testing_indexes = np.asarray(list(range(128, 256)))
+        random_walk.give_test_data(input[testing_indexes],
+                                   output[128:256])  # add input ?
+
+        # CLassify the dataset
+        misclassification = random_walk.classify_dataset()
+        results += misclassification
+        y_results = results / 100
+    plt.plot(x_results, y_results)
+    plt.show()
+
+def figure_2_experiment():
+    dataset_loader = NewsGroupDatasetLoader()
+    dataset_loader.load_dataset()
+    kernel = ClusterKernel()
+    input_, output = dataset_loader.get_full_dataset()
+    input_, output = data_utils.construct_one_vs_all(input_, output, 0)
+
+    x_results = [2, 4, 8, 16, 32, 64, 128]
+    y_results = []
+
+    for n_labeled_points in x_results:
+        print("Starting test for", n_labeled_points, "datapoints.")
+        results = 0
+        for i in range(100):
+            print("Iteration #" + str(i))
+
+            kernel_fun = kernel.kernel(input_)
+            svm = SVM()
+
+            #Send the data and unlabeled data (testing is analysed as unlabeled data)
+            svm.set_kernel(kernel_fun)
+
+            #Get the training indexes
+            training_indexes = np.asarray(list(range(128)))
+            training_targets_subset = []
+
+            #Make sure that the data has both 1 and -1
+            while 1 not in training_targets_subset or -1 not in training_targets_subset:
+                training_indexes_subset = np.random.choice(
+                    training_indexes, n_labeled_points)
+                training_targets_subset = output[
+                    training_indexes_subset]
+
+            #Give the data to the SVM
+            svm.give_training_data(training_indexes_subset,
+                                   training_targets_subset)
+
+            #Train the SVM.
+            svm.train()
+
+            #Send the indexes of labeled testing data and the labels
+            testing_indexes = np.asarray(list(range(128, 256)))
+            svm.give_test_data(testing_indexes, output[128:256])
+            misclassification = svm.analyze()
+            results += misclassification
+        y_results.append(results / 100)
+    plt.plot(x_results, y_results)
+    plt.show()
+
 
 @gin.configurable
 class ExperimentRunner:
@@ -49,94 +172,16 @@ class ExperimentRunner:
 
             train_svm(dataset_loader)
         elif self.experiment == 'figure_2':
+            figure_2_experiment()
+        elif self.experiment == 'random_walk':
+            random_walk_experiment()
+        elif self.experiment == 'transductive_svm':
             dataset_loader = NewsGroupDatasetLoader()
             dataset_loader.load_dataset()
-            kernel = ClusterKernel()
-            input_, output = dataset_loader.get_full_dataset()
-            input_, output = data_utils.construct_one_vs_all(input_, output, 0)
+            train_tsvm(dataset_loader)
 
-            x_results = [2, 4, 8, 16, 32, 64, 128]
-            y_results = []
-
-            for n_labeled_points in x_results:
-                print("Starting test for", n_labeled_points, "datapoints.")
-                results = 0
-                for i in range(100):
-                    print("Iteration #" + str(i))
-
-                    kernel_fun = kernel.kernel(input_)
-                    svm = SVM()
-
-                    #Send the data and unlabeled data (testing is analysed as unlabeled data)
-                    svm.set_kernel(kernel_fun)
-
-                    #Get the training indexes
-                    training_indexes = np.asarray(list(range(128)))
-                    training_targets_subset = []
-
-                    #Make sure that the data has both 1 and -1
-                    while 1 not in training_targets_subset or -1 not in training_targets_subset:
-                        training_indexes_subset = np.random.choice(
-                            training_indexes, n_labeled_points)
-                        training_targets_subset = output[
-                            training_indexes_subset]
-
-                    #Give the data to the SVM
-                    svm.give_training_data(training_indexes_subset,
-                                           training_targets_subset)
-
-                    #Train the SVM.
-                    svm.train()
-
-                    #Send the indexes of labeled testing data and the labels
-                    testing_indexes = np.asarray(list(range(128, 256)))
-                    svm.give_test_data(testing_indexes, output[128:256])
-                    misclassification = svm.analyze()
-                    results += misclassification
-                y_results.append(results / 100)
-            plt.plot(x_results, y_results)
-            plt.show()
-
-        elif self.experiment == 'random_walk':
-            datasetLoader = NewsGroupDatasetLoader()
-            datasetLoader.load_dataset()
-            input, output = datasetLoader.get_full_dataset()
-            input, output = data_utils.construct_one_vs_all(input, output, 0)
-
-            n_labeled_points = 16
-
-            print("Starting test for ", n_labeled_points, " datapoints.")
-            results = 0
-            for i in range(100):
-                random_walk = MRW()
-
-                print("Iteration #" + str(i))
-
-                #Get the training indexes
-                training_indexes = np.asarray(list(range(128)))
-                training_targets_subset = []
-
-                #Make sure that the data has both 1 and -1
-                while 1 not in training_targets_subset or -1 not in training_targets_subset:
-                    training_indexes_subset = np.random.choice(training_indexes, n_labeled_points)
-                    training_targets_subset = output[training_indexes_subset]
-
-                #Give the data to the random_walk
-                random_walk.give_training_data(input[training_indexes_subset],training_targets_subset)# add input ?
-
-                # Adding the test values
-                testing_indexes = np.asarray(list(range(128, 256)))
-                random_walk.give_test_data(input[testing_indexes], output[128:256]) # add input ?
-
-                # CLassify the dataset
-                misclassification = random_walk.classify_dataset()
-                results += misclassification
-                y_results = results / 100
-            plt.plot(x_results, y_results)
-            plt.show()
-
-            #Todo: Construct kernel for each type of kernel function.
-            #Todo: Average test error over 100 random selections of labelled points, ranging from 2->128.
-            #reuse the same kernel.
-            #(ie 100 runs with 2, 100 runs with 4, 8, 16, 32, 64, 128)
-            #Todo: Visualize and evaluate eigenvalue sizes also.
+        #Todo: Construct kernel for each type of kernel function.
+        #Todo: Average test error over 100 random selections of labelled points, ranging from 2->128.
+        #reuse the same kernel.
+        #(ie 100 runs with 2, 100 runs with 4, 8, 16, 32, 64, 128)
+        #Todo: Visualize and evaluate eigenvalue sizes also.
