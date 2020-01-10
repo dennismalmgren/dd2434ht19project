Corrected the misclassification calculation

src/svm.py
@@ -168,7 +168,7 @@ class SVM(object):
     def analyze(self):
         results = self.classify_dataset(self.test_data_in)
         classifications = results[:,0]
-        misclassification = np.abs(np.sum(classifications-self.test_data_out))/(2*len(classifications))
+        misclassification = np.sum(np.abs(classifications-self.test_data_out))/(2*len(classifications))
         print("Misclassification",misclassification)
         return misclassification
 