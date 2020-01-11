1954bcfaa1f3fd86bc0554ac7024b380b49db859
Yann POURCENOUX <yann.pourcenoux@gmail.com>
2020-01-11 21:10:26 +0100

    removed min_df

--- src/dataloaders/newsgroupdatasetloader.py
+++ src/dataloaders/newsgroupdatasetloader.py
@@ -1,4 +1,4 @@
-#Example code for clustering the dataset here: 
+#Example code for clustering the dataset here:
 # https://scikit-learn.org/stable/auto_examples/text/plot_document_clustering.html#sphx-glr-auto-examples-text-plot-document-clustering-py
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.datasets import fetch_20newsgroups_vectorized
@@ -21,7 +21,7 @@ Helper class for obtaining and transforming newsgroup 20 data points.
 There is no explicit support for visualization as it is all text data.
 Some helper functions for going from document, to vector, to category etc are missing.
 Additionally depending on algorithm support for splits and shuffling needs to be implemented.
-The class can return two types of primary data objects. Documents, which are textual, or vectors, 
+The class can return two types of primary data objects. Documents, which are textual, or vectors,
 which is a tdfif representation of the same documents.
 """
 @gin.configurable
@@ -32,17 +32,17 @@ class NewsGroupDatasetLoader:
         self.categories = categories
 
     """
-    Loads the data from the web for the required categories. 
+    Loads the data from the web for the required categories.
     This needs revising to work with the counting pipeline.
     """
     def load_dataset(self):
-        self.dataset = fetch_20newsgroups(subset='all', categories=self.categories, 
+        self.dataset = fetch_20newsgroups(subset='all', categories=self.categories,
                             download_if_missing=True, shuffle = self.shuffle, random_state = self.random_state)
         self.data = np.asarray(self.dataset.data)
         #Depending on if 2 or 3 categories are sent the dimensions change
         #however, only between 3-5k, not 7k... trying with all categories results in 18k.
         #an alternative would be rare word-removal (not yet implemented)
-        vectorizer = TfidfVectorizer(min_df=10)
+        vectorizer = TfidfVectorizer()
         vectors = vectorizer.fit_transform(self.data)
         self.vectors = vectors.toarray()
         self.targets = np.asarray(self.dataset.target)
@@ -56,7 +56,7 @@ class NewsGroupDatasetLoader:
 
 
     """
-    Returns the actual text documents for a category selected 
+    Returns the actual text documents for a category selected
     from this files category list.
 
     Args:
@@ -74,5 +74,3 @@ if __name__ =="__main__":
     setloader = NewsGroupDatasetLoader(categories=categories)
     setloader.load_dataset()
     dataset = setloader.get_full_dataset()
-
-
